68e83a1e6b68efee012e7d85166603da3c8361c3
Mateusz Szewczyk <szewczyk.mateusz16@gmail.com>
2021-09-08 16:03:38 +0200

    rename database

--- app/src/main/java/com/app/TODOapp/AppDatabase.kt
+++ app/src/main/java/com/app/TODOapp/AppDatabase.kt
@@ -24,7 +24,7 @@ abstract class AppDatabase : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "word_database"
+                    "task_database"
                 ).allowMainThreadQueries().fallbackToDestructiveMigration().build()
                 INSTANCE = instance
                 // return instance
